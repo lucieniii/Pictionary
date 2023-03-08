8279f5604391da9612af38932968ce17bfa17ca7
Linsonng <linsonng@163.com>
2023-03-08 03:01:50 +0000

    Update Pen.cs

--- Assets/Pen.cs
+++ Assets/Pen.cs
@@ -1,52 +1,66 @@
 using UnityEngine;
 using Ubiq.XR;
-using Ubiq.Messaging; // new
+using Ubiq.Messaging;
 
-public class Pen : MonoBehaviour, IGraspable
+// Adds simple networking to the 3d pen. The approach used is to draw locally
+// when a remote user tells us they are drawing, and stop drawing locally when
+// a remote user tells us they are not.
+public class Pen : MonoBehaviour, IGraspable, IUseable
 {
-    private NetworkContext context; // new
-    private bool owner; // new
+    private NetworkContext context;
+    private bool owner;
     private Hand controller;
+    private Transform nib;
+    private Material drawingMaterial;
+    private GameObject currentDrawing;
 
-    // new
-    // 1. Define a message format. Let's us know what to expect on send and recv
+    // Amend message to also store current drawing state
     private struct Message
     {
         public Vector3 position;
         public Quaternion rotation;
+        public bool isDrawing; // new
 
-        public Message(Transform transform)
+        public Message(Transform transform, bool isDrawing)
         {
             this.position = transform.position;
             this.rotation = transform.rotation;
+            this.isDrawing = isDrawing; // new
         }
     }
 
-    // new
     private void Start()
     {
-        // 2. Register the object with the network scene. This provides a
-        // NetworkID for the object and lets it get messages from remote users
+        nib = transform.Find("Grip/Nib");
         context = NetworkScene.Register(this);
+        var shader = Shader.Find("Particles/Standard Unlit");
+        drawingMaterial = new Material(shader);
     }
 
-    // new
     public void ProcessMessage (ReferenceCountedSceneGraphMessage msg)
     {
-        // 3. Receive and use transform update messages from remote users
-        // Here we use them to update our current position
         var data = msg.FromJson<Message>();
         transform.position = data.position;
         transform.rotation = data.rotation;
+
+        // new
+        // Also start drawing locally when a remote user starts
+        if (data.isDrawing && !currentDrawing)
+        {
+            BeginDrawing();
+        }
+        if (!data.isDrawing && currentDrawing)
+        {
+            EndDrawing();
+        }
     }
 
-    // new
     private void FixedUpdate()
     {
         if (owner)
         {
-            // 4. Send transform update messages if we are the current 'owner'
-            context.SendJson(new Message(transform));
+            // new
+            context.SendJson(new Message(transform,isDrawing:currentDrawing));
         }
     }
 
@@ -61,19 +75,46 @@ public class Pen : MonoBehaviour, IGraspable
 
     void IGraspable.Grasp(Hand controller)
     {
-        // 5. Define ownership as 'who holds the item currently'
-        owner = true; // new
+        owner = true;
         this.controller = controller;
     }
 
     void IGraspable.Release(Hand controller)
     {
-        // As 5. above, define ownership as 'who holds the item currently'
-        owner = false; // new
+        owner = false;
         this.controller = null;
     }
 
-     // Note about ownership: 'ownership' is just one way of designing this
-     // kind of script. It's sometimes a useful pattern, but has no special
-     // significance outside of this file or in Ubiq more generally.
+    void IUseable.Use(Hand controller)
+    {
+        BeginDrawing();
+    }
+
+    void IUseable.UnUse(Hand controller)
+    {
+        EndDrawing();
+    }
+
+    private void BeginDrawing()
+    {
+        currentDrawing = new GameObject("Drawing");
+        var trail = currentDrawing.AddComponent<TrailRenderer>();
+        trail.time = Mathf.Infinity;
+        trail.material = drawingMaterial;
+        trail.startWidth = .05f;
+        trail.endWidth = .05f;
+        trail.minVertexDistance = .02f;
+
+        currentDrawing.transform.parent = nib.transform;
+        currentDrawing.transform.localPosition = Vector3.zero;
+        currentDrawing.transform.localRotation = Quaternion.identity;
+    }
+
+    private void EndDrawing()
+    {
+        var trail = currentDrawing.GetComponent<TrailRenderer>();
+        currentDrawing.transform.parent = null;
+        currentDrawing.GetComponent<TrailRenderer>().emitting = false;
+        currentDrawing = null;
+    }
 }
