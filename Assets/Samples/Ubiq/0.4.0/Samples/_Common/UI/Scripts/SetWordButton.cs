f2959a3bb8854a47573cdecd30616bb8a6e6a409
Linsonng <linsonng@163.com>
2023-03-08 18:34:29 +0000

    [add] required panels

---
+++ Assets/Samples/Ubiq/0.4.0/Samples/_Common/UI/Scripts/SetWordButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ubiq.Samples.Social
+{
+    public class SetWordButton : MonoBehaviour
+    {
+        public NameManager wordManager;
+        public Text wordText;
+
+        // Expected to be called by a UI element    
+        public void SetName()
+        {
+            if (wordText && wordManager)
+            {
+                wordManager.SetName(wordText.text);
+            }
+        }
+    }
+}
