5eebe65881ce396524bb10132f9d8702fe6c6f3e
lucieniii <lucienli0320@gmail.com>
2023-03-13 00:46:25 +0000

    [dbg] fix yellow and white pen

---
+++ Assets/Scripts/pens/YellowPen.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Ubiq.XR;
+using Ubiq.Messaging;
+
+namespace DrawAndGuess.DrawAndGuess
+{
+    // Adds simple networking to the 3d pen. The approach used is to draw locally
+    // when a remote user tells us they are drawing, and stop drawing locally when
+    // a remote user tells us they are not.
+    public class YellowPen : Pen
+    {
+        // Override
+        private void Start()
+        {
+            nib = transform.Find("Grip/YellowNib");
+            context = NetworkScene.Register(this);
+            Debug.Log(context.Id);
+            // var shader = Shader.Find("Unlit/Color");
+            // drawingMaterial = new Material(shader);
+        }
+    }
+}
