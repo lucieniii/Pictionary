efd3aceb8805af3354c30faeb1b567478f8eef57
lucieniii <lucienli0320@gmail.com>
2023-03-13 00:42:55 +0000

    [dbg] fix pin pen

---
+++ Assets/Scripts/pens/PinkPen.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Ubiq.XR;
+using Ubiq.Messaging;
+
+namespace DrawAndGuess.DrawAndGuess
+{
+    public class PinkPen : Pen
+    {
+        private void Start()
+        {
+            nib = transform.Find("Grip/PinkNib");
+            context = NetworkScene.Register(this);
+            Debug.Log(context.Id);
+            // var shader = Shader.Find("Unlit/Color");
+            // drawingMaterial = new Material(shader);
+        }
+    }
+}
